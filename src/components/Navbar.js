2b46965b424fe53a668fa1cf33fd738911cb4c01
VladanStar <vladancupric@gmail.com>
2022-09-13 23:05:52 +0200

    Add NavBar in Header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({location}) => {
+
+   // navbar navbar-expand-lg bg-dark
+
+   let headerClass = '';
+
+   if(location !== '/'){
+       headerClass = 'navbar navbar-expand-lg bg-dark';
+   } else {
+       headerClass = 'navbar navbar-expand-lg fixed-top';
+   }
   return (
-    <div>Navbar</div>
+    <nav className={headerClass}>
+            <div className="container">
+                <a className="navbar-brand text-white fw-bold fst-italic" href="index.html"><img src="img/logo.png" alt="" width="50"/> TravelStar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link text-white" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item ">
+                            <Link className="nav-link text-white" to="putovanja">Putovanja</Link>
+                        </li>
+                        <li className="nav-item ">
+                            <Link className="nav-link text-white" to="dashboard">Dashboard</Link>
+                        </li>
+                        <li className="nav-item ">
+                            <Link className="nav-link text-white " to="detalji">Detalji</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
